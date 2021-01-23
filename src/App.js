10fb06accec100aa1de3f54d5b97f2e517aab037
devCecy <im.ceciliaan@gmail.com>
2021-01-24 02:57:28 +0900

    Redux example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement } from "./actions/action"
 
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const isLogged = useSelector((state) => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>count : {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>로그인되어야 볼수 있는 화면입니다</h3> : ""}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
