8bd8e74128541cb7b9be2fe7625746b77c35fc23
devCecy <im.ceciliaan@gmail.com>
2021-01-24 17:59:57 +0900

    Redux 흐름이해

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>count : {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(10))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? <h3>로그인되어야 볼수 있는 화면입니다</h3> : ""}
     </div>
--- src/actions/action.js
+++ src/actions/action.js
@@ -1,5 +1,5 @@
-export const increment = () => {
-  return { type: "INCREMENT" }
+export const increment = (num) => {
+  return { type: "INCREMENT", payload: num }
 }
 
 export const decrement = () => {
